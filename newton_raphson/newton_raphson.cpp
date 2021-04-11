e7b1b0e9f8a288f6cbe6140417fda6a822a1318f
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2021-04-11 19:14:26 -0300

    inserindo contador de tempo de exeucao

--- newton_raphson/newton_raphson.cpp
+++ newton_raphson/newton_raphson.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include "mpi.h"
 
 int main(int argc, char* argv[]) {
 
 	float raiz_newton_raphson(float x0);
+	float t_inicial,t_final;
 	
 	int myrank;
 	MPI_Status status;
@@ -15,7 +17,8 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
 	if (myrank == 0){
-		float x0 = 1000000000000.0;
+		float x0 = 9999999999999999;	
+		printf("X0 = %f\n", x0);
 		MPI_Send(&x0, 1, MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
 	}
 
@@ -24,10 +27,12 @@ int main(int argc, char* argv[]) {
 		float x0_recv = 0.;
 		
 		MPI_Recv(&x0_recv, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD,&status);
-
-		printf("X0 = %f ", x0_recv);
-
+				
+		t_inicial = clock();
 		float xn = raiz_newton_raphson(x0_recv);
+		t_final = clock() - t_inicial;
+		
+		printf("\nTempo de execucao: %lf\n", ((double)t_final) / ((CLOCKS_PER_SEC / 1000)));
 
 		MPI_Send(&xn, 1, MPI_FLOAT, 2, 0, MPI_COMM_WORLD);
 	}
@@ -37,7 +42,7 @@ int main(int argc, char* argv[]) {
 		float raiz_recv = 0.;
 		MPI_Recv(&raiz_recv, 1, MPI_FLOAT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
 
-		printf("raiz = %f ", raiz_recv);
+		printf("\nRaiz = %f\n", raiz_recv);
 	}
 
 	MPI_Finalize();
@@ -46,7 +51,7 @@ int main(int argc, char* argv[]) {
 
 
 float raiz_newton_raphson(float x0) {
-	
+		
 	int i = 0;
 	float xn = 0.0, fxn, raiz;
 	float eps = 1.0E-6;
@@ -57,6 +62,7 @@ float raiz_newton_raphson(float x0) {
 	for (i; f(xn) > eps; i++) {
 		xn = x0 - f(x0) / df(x0);
 		fxn = fabs(f(xn));
+		printf("Iteracao %d   |f(x)|: %10.6f\n", i, fxn);
 		x0 = xn;
 	}
 
